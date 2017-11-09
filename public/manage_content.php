53a1c4d5a35e4623efa8bd1fe52c93b66e1d1f2e
muhammadaziss07 <muhammadaziss07@users.noreply.github.com>
2017-11-09 23:37:27 +0700

    Refactoring the navigation

--- public/manage_content.php
+++ public/manage_content.php
@@ -5,29 +5,14 @@
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
-			<?php
-				$query = "SELECT * ";
-				$query .= "FROM subjects ";
-				$query .= "WHERE visible = 1 ";
-				$query .= "ORDER BY position ASC";
-				$subject_set = mysqli_query($connection, $query);
-				confirm_query($subject_set);
-			?>
+		<?php $subject_set = find_all_subjects(); ?>
 		<?php
 		while($subject = mysqli_fetch_assoc($subject_set)) {
 		?>  
 			<li>
 				<?php echo $subject["menu_name"]; ?>
-				<?php
-					$query = "SELECT * ";
-					$query .= "FROM pages  ";
-					$query .= "AND subject_id = {$subject["id"]} ";
-					$query .= "WHERE visible = 1 ";
-					$query .= "ORDER BY position ASC";
-					$page_set = mysqli_query($connection, $query);
-					confirm_query($page_set);
-				?>
-			<ul class="pages">
+				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
+				<ul class="pages">
 				<?php
 					while($page = mysqli_fetch_assoc($page_set)) {
 				?>  
